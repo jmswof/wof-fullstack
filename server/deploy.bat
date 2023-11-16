9c50cef3c5206b4933df75724cc3e37df8744d54
Jason Schwebke <148880476+jmswof@users.noreply.github.com>
2023-11-16 11:34:46 -0500

    automate deployment for wof-server

---
+++ server/deploy.bat
@@ -0,0 +1,14 @@
+@ECHO Off
+ECHO DEPLOY TO DEVELOPMENT SERVER
+CALL pm2 stop wof-express
+CALL pm2 delete wof-express
+DEL D:\server\Apache24\sites\wof-server\index.js
+DEL D:\server\Apache24\sites\wof-server\*.json
+DEL D:\server\Apache24\sites\wof-server\.env
+RMDIR /S /Q D:\server\Apache24\sites\wof-server\node_modules
+COPY index.js D:\server\Apache24\sites\wof-server
+COPY package.json D:\server\Apache24\sites\wof-server
+COPY .env D:\server\Apache24\sites\wof-server
+CALL npm install --prefix D:\server\Apache24\sites\wof-server
+CALL pm2 start D:\server\Apache24\sites\wof-server\index.js --name wof-express
+ECHO THE DEPLOYMENT IS A SUCCESS!
