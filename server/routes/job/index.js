314d26225be1d0703abc61f82fd8ded5daf3cc9e
Jason Schwebke <148880476+jmswof@users.noreply.github.com>
2023-11-23 13:45:13 -0500

    formatting updates

--- server/routes/job/index.js
+++ server/routes/job/index.js
@@ -6,98 +6,82 @@ const client = new MongoClient(dbURL, { family: 4 /* Node 17+ requirement */ });
 const database = client.db('world-of-floors');
 const products = database.collection('products');
 
-const initWs = (socketio, name) => {
+const initWs = (socketio, route) => {
 
-  const nsp = socketio.of(name);
+  const nsp = socketio.of(route);
 
-  nsp.on('connection', (socket) => {
-    admin.auth().verifyIdToken(socket.handshake.auth.token)
+  nsp.on('connection', async (socket) => {
+    await admin.auth().verifyIdToken(socket.handshake.auth.token)
       .then(async token => {
         // console.log(token);
         socket.emit('list', await products.find().toArray());
-        console.log(`[CONNECTION] /jobs: verified token, emit jobs payload to socket::list ${socket.id}`);
-        console.log(`[CONNECTION] /jobs: current clients size: ${nsp.sockets.size}`);
+        console.log(`[WS][CONNECTION] ${route}: verified token, emit jobs payload to socket::list ${socket.id}`);
+        console.log(`[WS][CONNECTION] ${route}: current clients size: ${nsp.sockets.size}`);
       })
       .catch(error => {
-        console.log(`[CONNECTION] /jobs: firebase error ${error.code}`);
-        console.log(`[CONNECTION] /jobs: emit error and then terminate this socket ${socket.id}`);
+        console.log(`[WS][CONNECTION] ${route}: firebase error ${error.code}`);
+        console.log(`[WS][CONNECTION] ${route}: emit error and then terminate this socket ${socket.id}`);
         socket.emit('error', 'Invalid credentials, terminating connection...');
         socket.disconnect();
       });
 
     socket.on('message', (data) => {
-      console.log(`[MESSAGE] /jobs: contents = ${data}`);
+      console.log(`[WS][MESSAGE] ${route}: contents = ${data}`);
     });
 
     socket.on('disconnect', () => {
-      console.log(`[DISCONNECT] /jobs: current clients size = ${nsp.sockets.size}`);
+      console.log(`[WS][DISCONNECT] ${route}: current clients size = ${nsp.sockets.size}`);
     });
 
-    socket.on('error', () => {
-      console.log("error");
+    socket.on('error', (error) => {
+      console.log(`[WS][ERROR] ${route}: error'd = ${error}`);
     });
   });
 
   return nsp;
 };
 
-const initRest = (wss, app, name) => {
+const initRest = (wss, app, route) => {
 
-    // GET /products (READ)
-    app.get('/jobs', cors(), async (request, response, next) => {
+    // GET /jobs (READ)
+    app.get(route, cors(), async (request, response, next) => {
         response.json(await products.find().toArray());
     });
 
-    // POST /products (CREATE)
-    app.post('/jobs', cors(), async (request, response, next) => {
-        console.log(request.body);
-        response.json(await products.insertOne(request.body));
-        const t = JSON.stringify(await products.find().toArray());
-
-        wss.clients.forEach((client, k) => {
-            client.send(t);
-        });
+    // POST /jobs (CREATE)
+    app.post(route, cors(), async (request, response, next) => {
+      console.log(request.body);
+      response.json(await products.insertOne(request.body));
     });
 
-    // PATCH /products (UPDATE)
-    app.patch('/jobs', cors(), async (request, response, next) => {
-        console.log(request.body);
-        response.json(await products.updateOne(
-            {_id: new ObjectId(request.body._id)},
-            { $set: { name: request.body.name, cost: request.body.cost}},
-            {}
-        ));
-        const t = JSON.stringify(await products.find().toArray());
-        wss.clients.forEach((client, k) => {
-            client.send(t);
-        });
+    // PATCH /jobs (UPDATE)
+    app.patch(route, cors(), async (request, response, next) => {
+      console.log(request.body);
+      response.json(await products.updateOne(
+        {_id: new ObjectId(request.body._id)},
+        { $set: { name: request.body.name, cost: request.body.cost}},
+        {}
+      ));
     });
 
-    // DELETE /products (DELETE)
-    app.delete('/jobs', cors(), async (request, response, next) => {
-        response.json(
-            await products.deleteMany({
-                _id: {
-                    $in: request.body.map(id => (new ObjectId(id)))
-                }
-            })
-        );
-
-        const t = JSON.stringify(await products.find().toArray());
-        wss.clients.forEach((client) => {
-            client.send(t);
-        });
+    // DELETE /jobs (DELETE)
+    app.delete(route, cors(), async (request, response, next) => {
+      response.json(
+        await products.deleteMany({
+          _id: { $in: request.body.map(id => (new ObjectId(id))) }
+        })
+      );
     });
 
-    console.log("Mapped job routes and socket namespace");
+    console.log(`[INIT] initialized REST and WS on ${route} for CRUD routes...`);
 };
 
-const initJobs = (socketio, app, name) => {
-    initRest(
-        initWs(socketio, name),
-        app,
-        name
-    );
+const initJobs = (socketio, app, route) => {
+  initRest(
+    initWs(socketio, route),
+    app,
+    route
+  );
 };
 
 module.exports = { initJobs };
--- server/routes/product/index.js
+++ server/routes/product/index.js
@@ -10,93 +10,86 @@ const client = new MongoClient(dbURL, {
 const database = client.db('world-of-floors');
 const products = database.collection('products');
 
-const initWs = (socketio, name) => {
-    
-    const nsp = socketio.of(name);
-    console.log("Socket size: " + nsp.sockets.size);
-    return nsp.on('connection', async (socket) => {
-        console.log(`socketio.of(${name})`)
+const initWs = (socketio, route) => {
+  const nsp = socketio.of(route);
 
-        socket.on('message', (data) => {
-            //console.log(`[MESSAGE] /products, message connection key: ${request.headers['sec-websocket-key']}, clients size: ${socketio.clients.size}`);
-            console.log(`[MESSAGE] /products, contents: ${data}`);
-        });
-      
-        socket.on('disconnect', () => {
-          console.log("[DISCONNECT] /products, socket size: " + nsp.sockets.size);
-            
-            //console.log(`[CLOSE] /products, closed connection key: ${request.headers['sec-websocket-key']}, clients size: ${socketio.clients.size}`);
-        });
-
-        socket.on('error', () => {
-          console.log("error");
-        })
-
-        socket.emit('list', await products.find().toArray());
-        console.log('emitted products to socket');
-        console.log("Socket size: " + nsp.sockets.size);
-        //console.log(`[CONNECTION] /products, emit list to key: ${request.headers['sec-websocket-key']}, clients size: ${socketio.clients.size}`);
-        //console.log(connection);
+  nsp.on('connection', async (socket) => {
+    socket.on('message', (data) => {
+      console.log(`[WS][MESSAGE] ${route}, contents: ${data}`);
     });
+
+    socket.on('disconnect', () => {
+      console.log(`[WS][DISCONNECT] ${route}, current clients size: ${nsp.sockets.size}`);
+    });
+
+    socket.on('error', () => {
+      console.log("error");
+    });
+
+    socket.emit('list', await products.find().toArray());
+    console.log('emitted products to socket');
+    console.log("Socket size: " + nsp.sockets.size);
+  });
+
+  return nsp;
 };
 
-const initRest = (wss, app, name) => {
+const initRest = (wss, app, route) => {
 
-    // GET /products (READ)
-    app.get('/products', cors(), async (request, response, next) => {
-        response.json(await products.find().toArray());
-    });
+  // GET /products (READ)
+  app.get(route, cors(), async (request, response, next) => {
+    response.json(await products.find().toArray());
+  });
 
     // POST /products (CREATE)
-    app.post('/products', cors(), async (request, response, next) => {
-        console.log(request.body);
-        response.json(await products.insertOne(request.body));
-        const t = JSON.stringify(await products.find().toArray());
+  app.post(route, cors(), async (request, response, next) => {
+    console.log(request.body);
+    response.json(await products.insertOne(request.body));
+    const t = JSON.stringify(await products.find().toArray());
 
-        wss.clients.forEach((client, k) => {
-            client.send(t);
-        });
+    wss.clients.forEach((client, k) => {
+      client.send(t);
     });
+  });
 
-    // PATCH /products (UPDATE)
-    app.patch('/products', cors(), async (request, response, next) => {
-        console.log(request.body);
-        response.json(await products.updateOne(
-            {_id: new ObjectId(request.body._id)},
-            { $set: { name: request.body.name, cost: request.body.cost}},
-            {}
-        ));
-        const t = JSON.stringify(await products.find().toArray());
-        wss.clients.forEach((client, k) => {
-            client.send(t);
-        });
+  // PATCH /products (UPDATE)
+  app.patch(route, cors(), async (request, response, next) => {
+    console.log(request.body);
+    response.json(await products.updateOne(
+      {_id: new ObjectId(request.body._id)},
+      { $set: { name: request.body.name, cost: request.body.cost}},
+      {}
+    ));
+
+    const t = JSON.stringify(await products.find().toArray());
+    wss.clients.forEach((client, k) => {
+      client.send(t);
     });
+  });
 
-    // DELETE /products (DELETE)
-    app.delete('/products', cors(), async (request, response, next) => {
-        response.json(
-            await products.deleteMany({
-                _id: {
-                    $in: request.body.map(id => (new ObjectId(id)))
-                }
-            })
-        );
+  // DELETE /products (DELETE)
+  app.delete(route, cors(), async (request, response, next) => {
+    response.json(
+      await products.deleteMany({
+        _id: { $in: request.body.map(id => (new ObjectId(id))) }
+      })
+    );
 
-        const t = JSON.stringify(await products.find().toArray());
-        wss.clients.forEach((client) => {
-            client.send(t);
-        });
+    const t = JSON.stringify(await products.find().toArray());
+    wss.clients.forEach((client) => {
+      client.send(t);
     });
+  });
 
-    console.log("Mapped product routes and socket namespace");
+    console.log(`[INIT] initialized REST and WS on ${route} for CRUD routes...`);
 };
 
-const initProducts = (socketio, app, name) => {
-    initRest(
-        initWs(socketio, name),
-        app,
-        name
-    );
+const initProducts = (socketio, app, route) => {
+  initRest(
+    initWs(socketio, route),
+    app,
+    route
+  );
 };
 
 module.exports = { initProducts };
