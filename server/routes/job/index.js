754b2cabdbb5435ab1c02c81f13742aa1f41f8b5
Jason Schwebke <148880476+jmswof@users.noreply.github.com>
2023-11-23 00:07:13 -0500

    formatting updates

--- server/routes/job/index.js
+++ server/routes/job/index.js
@@ -1,58 +1,49 @@
 const cors = require('cors');
 const { MongoClient, ObjectId } = require("mongodb");
-const admin = require("firebase-admin")
+const admin = require("firebase-admin");
+
 // https://firebase.google.com/docs/admin/setup
-const config = require("../wof-server.json")
-admin.initializeApp( { credential: admin.credential.cert(config) } )
+const config = require("../../wof-server.json");
+admin.initializeApp( { credential: admin.credential.cert(config) } );
 
 const dbURL = process.env.WOF_DATABASE;
-const client = new MongoClient(dbURL, {
-  family: 4 // Node 17+ requirement
-});
-
+const client = new MongoClient(dbURL, { family: 4 /* Node 17+ requirement */ });
 const database = client.db('world-of-floors');
 const products = database.collection('products');
 
 const initWs = (socketio, name) => {
-    
-    const nsp = socketio.of(name);
-    console.log("Socket size: " + nsp.sockets.size);
-    return nsp.on('connection', async (socket) => {
-        console.log(`socketio.of(${name})`)
 
-        socket.on('message', (data) => {
-            //console.log(`[MESSAGE] /products, message connection key: ${request.headers['sec-websocket-key']}, clients size: ${socketio.clients.size}`);
-            console.log(`[MESSAGE] /products, contents: ${data}`);
-        });
-      
-        socket.on('disconnect', () => {
-          console.log("[DISCONNECT] /jobs, socket size: " + nsp.sockets.size);
-            
-            //console.log(`[CLOSE] /products, closed connection key: ${request.headers['sec-websocket-key']}, clients size: ${socketio.clients.size}`);
-        });
+  const nsp = socketio.of(name);
 
-        socket.on('error', () => {
-          console.log("error");
-        })
-
+  nsp.on('connection', (socket) => {
+    admin.auth().verifyIdToken(socket.handshake.auth.token)
+      .then(async token => {
+        // console.log(token);
         socket.emit('list', await products.find().toArray());
-        console.log('emitted jobs to socket');
-        console.log("Socket size: " + nsp.sockets.size);
-        admin
-          .auth()
-          .verifyIdToken(socket.handshake.auth.token)
-          .then(token => {
-            console.log(token);
-          })
-          .catch(error => {
-            console.log(`[CONNECTION] /jobs ERROR ${error.code}`);
-            console.log(`[CONNECTION] /jobs TERMINATE SOCKET ${socket.id}`);
-            socket.emit('error', 'Invalid credentials, terminating connection...');
-            socket.disconnect();
-          })
-        //console.log(`[CONNECTION] /products, emit list to key: ${request.headers['sec-websocket-key']}, clients size: ${socketio.clients.size}`);
-        //console.log(connection);
+        console.log(`[CONNECTION] /jobs: verified token, emit jobs payload to socket::list ${socket.id}`);
+        console.log(`[CONNECTION] /jobs: current clients size: ${nsp.sockets.size}`);
+      })
+      .catch(error => {
+        console.log(`[CONNECTION] /jobs: firebase error ${error.code}`);
+        console.log(`[CONNECTION] /jobs: emit error and then terminate this socket ${socket.id}`);
+        socket.emit('error', 'Invalid credentials, terminating connection...');
+        socket.disconnect();
+      });
+
+    socket.on('message', (data) => {
+      console.log(`[MESSAGE] /jobs: contents = ${data}`);
     });
+
+    socket.on('disconnect', () => {
+      console.log(`[DISCONNECT] /jobs: current clients size = ${nsp.sockets.size}`);
+    });
+
+    socket.on('error', () => {
+      console.log("error");
+    });
+  });
+
+  return nsp;
 };
 
 const initRest = (wss, app, name) => {
