cdfe06881a787184bf84f3c8f6d27487289d18a0
Jason Schwebke <148880476+jmswof@users.noreply.github.com>
2023-11-27 10:19:16 -0500

    client to install npm

--- client/deploy.bat
+++ client/deploy.bat
@@ -4,7 +4,7 @@ DEL D:\server\Apache24\sites\wof-client\index.html
 DEL D:\server\Apache24\sites\wof-client\*.js
 RMDIR /S /Q D:\server\Apache24\sites\wof-client\static
 RMDIR /S /Q D:\server\Apache24\sites\wof-client\node_modules
+CALL npm install
 ROBOCOPY dist D:\server\Apache24\sites\wof-client /E
-CALL npm install --prefix D:\server\Apache24\sites\wof-client
 RMDIR /S /Q dist
 ECHO THE DEVELOPMENT WOF-CLIENT DEPLOYMENT IS A SUCCESS!
