eeefc6c54e66cd200aef5e757a2fcc18a8770801
Jason Schwebke <148880476+jmswof@users.noreply.github.com>
2023-11-27 10:29:20 -0500

    client webpack

--- client/deploy.bat
+++ client/deploy.bat
@@ -3,8 +3,8 @@ ECHO DEPLOY TO DEVELOPMENT WOF-CLIENT
 DEL D:\server\Apache24\sites\wof-client\index.html
 DEL D:\server\Apache24\sites\wof-client\*.js
 RMDIR /S /Q D:\server\Apache24\sites\wof-client\static
-RMDIR /S /Q D:\server\Apache24\sites\wof-client\node_modules
 CALL npm install
+CALL webpack .
 ROBOCOPY dist D:\server\Apache24\sites\wof-client /E
 RMDIR /S /Q dist
 ECHO THE DEVELOPMENT WOF-CLIENT DEPLOYMENT IS A SUCCESS!
